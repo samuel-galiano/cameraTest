966486ccf30e6aa575b341823af6127820dbd9ef
Samuel Ubuntu <2211600020@fema.edu.br>
2024-10-07 15:04:09 -0300

    feat: utilizar a camera do celular

--- App.style.tsx
+++ App.style.tsx
@@ -3,8 +3,29 @@ import { StyleSheet } from "react-native"
 export const AppStyles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
       justifyContent: 'center',
+    },
+    message: {
+      textAlign: 'center',
+      paddingBottom: 10,
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      backgroundColor: 'transparent',
+      margin: 64,
+    },
+    button: {
+      flex: 1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
     },
   });
--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
 import { AppStyles as styles } from "./App.style"
+import { Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  const [ladoCamera, setladoCamera] = useState<CameraType>('back');
+  const [permissao, requestpermissao] = useCameraPermissions();
+
+  if (!permissao) {
+    return <View />;
+  } else if (!permissao.granted) {
+    return (
+      <View style={styles.container}>
+          <Text style={styles.message}>Precisamos de permissão para acessar a camera do dispositivo</Text>
+          <TouchableOpacity onPress={requestpermissao}>
+              <Text>Liberar Permissão</Text>
+          </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+      <CameraView style={styles.camera} facing={ladoCamera}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => setladoCamera(current => (current === 'back' ? 'front' : 'back'))}>
+            <Text style={styles.text}>Virar Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
